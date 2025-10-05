389719e68197bfce7af0e23dfaf6b9075631e590
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-10-05 18:47:04 +0200

    feat: added bulbasaur in the scene

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Canvas } from "@react-three/fiber";
 import "./index.css";
 import Squirtle from "./components/Squirtle";
 import Charmander from "./components/Charmander";
+import Bulbasaur from "./components/Bulbasaur";
 import { OrbitControls } from "@react-three/drei";
 
 function App() {
@@ -13,8 +14,9 @@ function App() {
 				<ambientLight intensity={0.5} />
 				<directionalLight position={[0, 1, 1]} color={"#c10404"} intensity={2.5} />
 				<directionalLight position={[-4, 4, 1]} color={"#9214d1"} intensity={2.5} />
+				<Charmander position={[-0.7, -0.5, -0.2]} />
 				<Squirtle position={[0, -0.4, 0]} />
-				<Charmander position={[-0.7, -0.5, 0]} />
+				<Bulbasaur position={[0.7, -0.4, -0.2]} />
 			</Canvas>
 		</>
 	);
