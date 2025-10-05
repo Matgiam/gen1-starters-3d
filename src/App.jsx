fecd77699629f4d989a84289ddea6492d227a87c
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-10-05 18:43:17 +0200

    feat: added charmander the same way as squirtle and improved the directional lights color and intensity

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import { useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import "./index.css";
 import Squirtle from "./components/Squirtle";
+import Charmander from "./components/Charmander";
 import { OrbitControls } from "@react-three/drei";
 
 function App() {
 	return (
 		<>
-			<Canvas camera={{ position: [0, 0, 1], fov: 100 }} style={{ width: "100vw", height: "100vh" }}>
+			<Canvas camera={{ position: [0, 0, 1.5], fov: 100 }} style={{ width: "100vw", height: "100vh" }}>
 				<OrbitControls />
 				<ambientLight intensity={0.5} />
-				<directionalLight position={[0, 1, 1]} color={"#9214d1"} />
+				<directionalLight position={[0, 1, 1]} color={"#c10404"} intensity={2.5} />
 				<directionalLight position={[-4, 4, 1]} color={"#9214d1"} intensity={2.5} />
 				<Squirtle position={[0, -0.4, 0]} />
+				<Charmander position={[-0.7, -0.5, 0]} />
 			</Canvas>
 		</>
 	);
