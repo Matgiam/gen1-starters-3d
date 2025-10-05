27315ebefe0dfe2064c4a41a4ad3b171bc1b1af3
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-10-05 18:34:49 +0200

    feat: added OrbitControls to move around the model

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import "./index.css";
 import Squirtle from "./components/Squirtle";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
 	return (
 		<>
 			<Canvas camera={{ position: [0, 0, 1], fov: 100 }} style={{ width: "100vw", height: "100vh" }}>
+				<OrbitControls/>
 				<ambientLight intensity={0.5} />
 				<Squirtle position={[0, -0.4, 0]} />
 			</Canvas>
