1ee2152e49d3735caf183b3f988809b769000c74
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-10-06 09:40:35 +0200

    feat: added a rotating effect on the objects + disabled the fact that you could drag the object by right-clicking using enablePan={false}

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Canvas } from "@react-three/fiber";
+import { useState, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import "./index.css";
 import Squirtle from "./components/Squirtle";
 import Charmander from "./components/Charmander";
@@ -8,9 +8,26 @@ import Platform from "./components/Round_platform";
 import { OrbitControls, Environment } from "@react-three/drei";
 
 function App() {
+	const Rotating = () => {
+		const ref = useRef();
+		useFrame(() => {
+			if (ref.current) {
+				ref.current.rotation.y -= 0.001;
+			}
+		});
+		return (
+			<mesh ref={ref}>
+				<Charmander position={[-0.7, -0.51, -0.3]} />
+				<Squirtle position={[0, -0.4, 0]} />
+				<Bulbasaur position={[0.7, -0.4, -0.3]} />
+				<Platform position={[0, -0.435, -0.2]} />
+			</mesh>
+		);
+	};
+
 	return (
 		<>
-			<Canvas camera={{ position: [0, 0, 1.5], fov: 100 }} style={{ width: "100vw", height: "100vh" }}>
+			<Canvas camera={{ position: [0, 0, 1.7], fov: 100 }} style={{ width: "100vw", height: "100vh" }}>
 				<Environment files={"../images/clouds.jpeg"} background blur={0.1} backgroundRotation={[0, 1, 0]} />
 				<OrbitControls
 					// Limits how far the camera can tilt vertically (up/down)
@@ -22,16 +39,15 @@ function App() {
 					maxAzimuthAngle={0.7} // Rotate limit to the right
 					// Controls zoom distance between camera and model
 					minDistance={1} // How close you can zoom in
-					maxDistance={2} // How far you can zoom out
+					maxDistance={2.5} // How far you can zoom out
+					enablePan={false} //Disable the fact that you can drag the object
+					
 				/>
 
 				<directionalLight position={[0, 1, 1]} color={"#c10404"} intensity={0.5} />
 				<directionalLight position={[-4, 4, 1]} color={"#9214d1"} intensity={2.5} />
 				<directionalLight position={[0, 4, -2]} color={"#af7500"} intensity={1.5} />
-				<Charmander position={[-0.7, -0.51, -0.2]} />
-				<Squirtle position={[0, -0.4, 0]} />
-				<Bulbasaur position={[0.7, -0.4, -0.2]} />
-				<Platform position={[0, -0.435, -0.4]} />
+				<Rotating />
 			</Canvas>
 		</>
 	);
