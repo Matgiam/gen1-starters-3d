0e1110a67e9940ec739682b22ae1c5bdedf42064
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-10-14 09:45:20 +0200

    feat: converted glb models into jsx components to use them + made components for the scene to have more code readability

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -9,6 +9,7 @@ import Charmeleon from "./Charmeleon";
 import Charizard from "./Charizard";
 
 import DATA from "../data/Pokemons.json";
+import Scene1 from "./Scene1";
 
 function Scene() {
 	const [pokemon, setPokemon] = useState("");
@@ -24,42 +25,7 @@ function Scene() {
 		);
 	}
 	if (pokemon === "Charmander") {
-		const pokeData = DATA.find((p) => p.pokemon === "Charmander");
-		return (
-			<group>
-				<Charmander position={[0, -0.51, 0.2]} />
-				<Charmeleon position={[-4, -0.34, -2]} />
-				<Charizard position={[4.3, -0.39, -3]} />
-				<Platform position={[4, -0.435, -3]} />
-				<Platform position={[-4, -0.435, -2]} />
-				<Platform position={[0, -0.435, -0.3]} />
-				<Html distanceFactor={3} position={[-0.05, 0.1, 0.1]}>
-					<div className="name">
-						<h2>{pokeData.pokemon}</h2>
-					</div>
-					<div className="description">
-						<p>{pokeData.description}</p>
-					</div>
-					<div className="details">
-						<p>
-							<strong>Type:</strong> {pokeData.type}
-						</p>
-						<p>
-							<strong>Height:</strong> {pokeData.height}
-						</p>
-						<p>
-							<strong>Weight:</strong> {pokeData.weight}
-						</p>
-						<p>
-							<strong>Weaknesses:</strong> {pokeData.weaknesses}
-						</p>
-					</div>
-					<div className="pokemon-button">
-						<button onClick={() => setPokemon("")}>Go Back</button>
-					</div>
-				</Html>
-			</group>
-		);
+		return <Scene1 />;
 	}
 }
 
---
+++ src/components/Scene1.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { Html } from "@react-three/drei";
+import DATA from "../data/Pokemons.json";
+import Squirtle from "./Squirtle";
+import Charmander from "./Charmander";
+import Bulbasaur from "./Bulbasaur";
+import Platform from "./Round_platform";
+import Charmeleon from "./Charmeleon";
+import Charizard from "./Charizard";
+
+function Scene1() {
+    const pokeData = DATA.find((p) => p.pokemon === "Charmander");
+	return (
+		<group>
+			<Charmander position={[0, -0.51, 0.2]} />
+			<Charmeleon position={[-4, -0.34, -2]} />
+			<Charizard position={[4.3, -0.39, -3]} />
+			<Platform position={[4, -0.435, -3]} />
+			<Platform position={[-4, -0.435, -2]} />
+			<Platform position={[0, -0.435, -0.3]} />
+			<Html distanceFactor={3} position={[-0.05, 0.1, 0.1]}>
+				<div className="name">
+					<h2>{pokeData.pokemon}</h2>
+				</div>
+				<div className="description">
+					<p>{pokeData.description}</p>
+				</div>
+				<div className="details">
+					<p>
+						<strong>Type:</strong> {pokeData.type}
+					</p>
+					<p>
+						<strong>Height:</strong> {pokeData.height}
+					</p>
+					<p>
+						<strong>Weight:</strong> {pokeData.weight}
+					</p>
+					<p>
+						<strong>Weaknesses:</strong> {pokeData.weaknesses}
+					</p>
+				</div>
+				<div className="pokemon-button">
+					<button onClick={() => setPokemon("")}>Go Back</button>
+				</div>
+			</Html>
+		</group>
+	);
+}
+
+export default Scene1;
